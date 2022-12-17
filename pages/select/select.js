71d525d9ead77e57b40d1f486f97b8854b037ff6
Xinwen Yu <xinwen.yu@flexiv.com>
2022-12-17 23:26:00 +0800

    finish select view page

--- pages/select/select.js
+++ pages/select/select.js
@@ -9,108 +9,28 @@ Page({
     data: {
         leftStyle: 'menu-select',
         rightStyle: 'menu-noselect',
-        hotSelect: [
-            {
-                title: '聚餐吃什么？',
-                display: false,
-                selects: [
-                    { 'name': "日本料理" },
-                    { 'name': "火锅" },
-                    { 'name': "自己多少斤心里没点数" },
-                    { 'name': "泰国菜" },
-                    { 'name': "海鲜" },
-                    { 'name': "烤鱼" },
-                    { 'name': "拉面" },
-                    { 'name': "麻辣烫" },
-                    { 'name': "自助餐" },
-                ]
-            },
-            {
-                title: '今天谁买单？',
-                display: false,
-                selects: [
-                    { "name": "身高最高的" },
-                    { "name": "微信好友最多的" },
-                    { "name": "年龄最小的" },
-                    { "name": "年龄最大的" },
-                    { "name": "头发最短的" },
-                    { "name": "头发最长的" },
-                ]
-            },
-            {
-                title: '真心话',
-                display: false,
-                selects: [
-                    { "name": "描述经历过最尴尬的事" },
-                    { "name": "带过异性回家吗" },
-                    { "name": "第一次恋爱几岁" },
-                    { "name": "做过最疯狂的事是什么" },
-                    { "name": "单身的感觉好吗" },
-                    { "name": "讲一下你的最近一次分手" },
-                    { "name": "与前任还有联系吗" },
-                    { "name": "你最喜欢的异性风格" },
-                    { "name": "谈过几次恋爱" },
-                    { "name": "多久没看片了" },
-                ]
-            },
-            {
-                title: '狼人杀身份牌',
-                display: false,
-                selects: [
-                    { "name": "预言家" },
-                    { "name": "狼人" },
-                    { "name": "村民" },
-                    { "name": "狼人" },
-                    { "name": "村民" },
-                    { "name": "狼人" },
-                    { "name": "村民" },
-                    { "name": "村民" },
-                    { "name": "猎人" },
-                    { "name": "女巫" },
-                    { "name": "丘比特" },
-                ]
-            },
-            {
-                title: '大冒险',
-                display: false,
-                selects: [
-                    { "name": "公主抱离你最近的异性" },
-                    { "name": "向一位同性表白3分钟" },
-                    { "name": "向一位异性表白3分钟" },
-                    { "name": "脱一件衣服至游戏结束" },
-                    { "name": "做一个最性感的动作" },
-                    { "name": "与异性十指相扣，对视十秒" },
-                ]
-            },
-            {
-                title: '骰子',
-                display: false,
-                selects: [
-                    { "name": "1" },
-                    { "name": "2" },
-                    { "name": "3" },
-                    { "name": "4" },
-                    { "name": "5" },
-                    { "name": "6" },
-                ]
-            },
-
-
-        ]
+        hotSelect: [],
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+    //     console.log(app.hotSelect,app)
+    //     this.data.hotSelect=app.hotSelect
+    //     console.log(this.data)
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady() {
-
+        console.log(app.hotSelect,app)
+        this.data.hotSelect=app.hotSelect
+        console.log(this.data)
+        this.setData({
+            'hotSelect':this.data.hotSelect
+        })
     },
 
     /**
@@ -156,12 +76,12 @@ Page({
     },
     selectTurnTable(event) {
         console.log(event)
-        const selectIndex = event.currentTarget.dataset.type
+        const selectIndex = event.currentTarget.dataset.titleindex
         app.awardsConfig = {
             chance: true,
-            awards: this.data.hotSelect[selectIndex].selects,
-            title: this.data.hotSelect[selectIndex].title,
-            subTitle: this.data.hotSelect[selectIndex].subTitle,
+            awards: app.hotSelect[selectIndex].selects,
+            title: app.hotSelect[selectIndex].title,
+            subTitle: app.hotSelect[selectIndex].subTitle,
         }
         wx.reLaunch({
             url: '/pages/index/index',
@@ -170,14 +90,20 @@ Page({
     },
     dropSelect(event){
         console.log("event", event)
-        const selectIndex = event.currentTarget.dataset.type
-        console.log('selectIndex', selectIndex)
-        console.log(this.data.hotSelect[selectIndex].display)
-        this.data.hotSelect[selectIndex]['display'] = !this.data.hotSelect[selectIndex].display
+        const titleIndex = event.currentTarget.dataset.titleindex
+        console.log(app.hotSelect[titleIndex].display)
+        app.hotSelect[titleIndex]['display'] = !app.hotSelect[titleIndex].display
         console.log('data',this.data)
         this.setData({
-            hotSelect:this.data.hotSelect,
+            hotSelect:app.hotSelect,
             // "title.display": this.data.hotSelect[selectIndex].display
         })
-    }
+    },
+    inputBlur(event){
+        console.log("event", event)
+        const titleIndex = event.currentTarget.dataset.titleindex
+        const subIndex = event.currentTarget.dataset.subindex
+        console.log(titleIndex,app.hotSelect[titleIndex],event.detail.value)
+        app.hotSelect[titleIndex]['selects'][subIndex].name = event.detail.value
+    },
 })
